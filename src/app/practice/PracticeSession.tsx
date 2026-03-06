523d01acc8e75bc3fdf7fef128df0bf209fc324e
Joo Eon <jooeon427@gmail.com>
2026-03-06 16:08:00 -0500

    - use dynamic viewport height for mobile devices

--- src/app/practice/PracticeSession.tsx
+++ src/app/practice/PracticeSession.tsx
@@ -96,7 +96,7 @@ export default function PracticeSession({ pins, rounds, timePerImage, warnInterv
     // console.log("queue:", queue.map(p => p.id));
 
     return (
-        <div className="min-h-[100vh] relative flex flex-col items-center">
+        <div className="h-dvh xl:min-h-[100vh] relative flex flex-col items-center">
             <div className="relative h-fit">
                 <img src={currentPin.media.images["1200x"].url} alt="current_practice_image"
                      className={`object-cover max-h-[90vh] xl:max-h-[100vh] xl:h-screen ${isPaused ? 'opacity-75' : 'opacity-100'}`} />
